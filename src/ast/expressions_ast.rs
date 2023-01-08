9308fb39c64cf4342224b51d40fcccb8d233e39e
KanHar <knaan.harpaz@gmail.com>
2023-01-08 16:59:37 +0200

    WIP

--- src/ast/expressions_ast.rs
+++ src/ast/expressions_ast.rs
@@ -3,10 +3,14 @@ use std::sync::Arc;
 pub enum ExpAst {
     U {
         i: usize,
-    }, // The i th type universe
+    }, // The i th type universe, starting with 0
     Void,      // The empty type
     Unit,      // The unit type
     Singleton, // The singleton, element of the unit type
+    Func {
+        domain: Arc<ExpAst>,
+        codomain: Arc<ExpAst>,
+    }, // The type of functions from domain to codomain
     Pi {
         base: Arc<ExpAst>,
         body: Arc<ExpAst>,
@@ -25,7 +29,9 @@ pub enum ExpAst {
     }, // Sum (either, coproduct, disjoint union) type
     Eq {
         t: Arc<ExpAst>,
-    }, // Equality =_t (identity) type
+        left: Arc<ExpAst>,
+        right: Arc<ExpAst>,
+    }, // Equality =_t (identity) type, left =_t right
     W {
         base: Arc<ExpAst>,
         body: Arc<ExpAst>,
